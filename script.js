3af47832609e2dd08b6a1f303ab222ec0a3b6686
Colin Whitcomb <colinsw@berkeley.edu>
2020-06-24 23:11:39 -0700

    Updated local Storage process.

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
 $(document).ready(function () {
 
+
+    
             $('.saveBtn').on("click", function () {
 
                 var toDo = $(this).siblings(".description").val();
-                console.log(toDo);
+                // console.log("todo" + toDo);
                 var hour = $(this).parent().attr("id");
                 // console.log(hour);
                 localStorage.setItem(hour, toDo);
+                var gotWhatUserWrote = localStorage.getItem(hour);
+                console.log("We got -> " + gotWhatUserWrote);
+                if (gotWhatUserWrote !== "") {
+                    $("id").text(gotWhatUserWrote);
+                }
+               
             });
 
             $('#currentDay').text(moment().format("dddd, MMM Do YYYY"));
 
+            
             function giveColor() {
 
                 $(".time-block").each(function () {
                     console.log("each ran");
 
                     var currentTime = moment().hour();
-                    console.log("This is current time: " + currentTime);
+                    // console.log("This is current time: " + currentTime);
 
                     var hour = parseInt($(this).attr("id"));
                     console.log(hour);
@@ -38,8 +47,8 @@ $(document).ready(function () {
                 })
 
             }
-
-
+          
+    
 
             giveColor();
         })
